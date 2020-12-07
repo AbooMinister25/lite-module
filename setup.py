4ed6881a92ba6b1177b9b62f705cf7f7f98bf06a
Rayyan Cyclegar <aboominister@gmail.com>
2020-12-07 17:11:06 -0500

    New Commit

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as f:
+      long_description = f.read()
+
 setup(name='lite-module',
-      version='0.0.1',
+      version='0.0.2',
       description='Python package for creating Lite modules',
+      long_description = long_description,
+      long_description_content_type="text/markdown",
       author='Rayyan Cyclegar',
       author_email='aboominister@gmail.com',
       license='MIT',
